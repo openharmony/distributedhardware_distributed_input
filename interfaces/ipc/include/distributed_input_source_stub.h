50b6a3be8844028a9a3c3a77d7f737ba9c377f7c
wuqi0105 <wuqi100@h-partners.com>
2023-08-21 19:00:28 +0800

    修改圈复杂度
    Signed-off-by: wuqi0105 <wuqi100@h-partners.com>

--- interfaces/ipc/include/distributed_input_source_stub.h
+++ interfaces/ipc/include/distributed_input_source_stub.h
@@ -65,7 +65,7 @@ private:
     std::mutex operatorMutex_;
     using DistributedInputSourceFunc = int32_t (DistributedInputSourceStub::*)(MessageParcel &data,
         MessageParcel &reply);
-    std::map<int32_t, DistributedInputSourceFunc> memberFuncMap_;
+    std::map<uint32_t, DistributedInputSourceFunc> memberFuncMap_;
 };
 } // namespace DistributedInput
 } // namespace DistributedHardware
