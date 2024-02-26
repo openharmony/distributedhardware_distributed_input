cc32252c8a41294c50d3e70785cf44475e6ea4e5
li-tiangang4 <litiangang4@huawei.com>
2024-02-26 11:04:04 +0800

    sa串行化
    Signed-off-by: li-tiangang4 <litiangang4@huawei.com>

--- interfaces/ipc/src/start_stop_d_inputs_call_back_stub.cpp
+++ interfaces/ipc/src/start_stop_d_inputs_call_back_stub.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -24,9 +24,13 @@
 namespace OHOS {
 namespace DistributedHardware {
 namespace DistributedInput {
-StartStopDInputsCallbackStub::StartStopDInputsCallbackStub() {}
+StartStopDInputsCallbackStub::StartStopDInputsCallbackStub() : IRemoteStub(true)
+{
+}
 
-StartStopDInputsCallbackStub::~StartStopDInputsCallbackStub() {}
+StartStopDInputsCallbackStub::~StartStopDInputsCallbackStub()
+{
+}
 
 int32_t StartStopDInputsCallbackStub::OnRemoteRequest(
     uint32_t code, MessageParcel &data, MessageParcel &reply, MessageOption &option)
