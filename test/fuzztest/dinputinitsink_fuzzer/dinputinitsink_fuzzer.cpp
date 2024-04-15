dc4f506663203e24e2b4f837888d64cb57be6d0a
li-tiangang4 <litiangang4@huawei.com>
2024-04-15 15:57:32 +0800

    修改异常fuzz用例
    Signed-off-by: li-tiangang4 <litiangang4@huawei.com>

--- test/fuzztest/dinputinitsink_fuzzer/dinputinitsink_fuzzer.cpp
+++ test/fuzztest/dinputinitsink_fuzzer/dinputinitsink_fuzzer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -20,6 +20,7 @@
 #include <functional>
 #include <iostream>
 #include <thread>
+#include <unistd.h>
 
 #include <refbase.h>
 
@@ -38,6 +39,8 @@ void InitSinkFuzzTest(const uint8_t *data, size_t size)
     }
     std::string params(reinterpret_cast<const char*>(data), size);
     DistributedInput::DistributedInputSinkHandler::GetInstance().InitSink(params);
+    const uint32_t sleepTimeUs = 50 * 1000;
+    usleep(sleepTimeUs);
 }
 } // namespace DistributedHardware
 } // namespace OHOS
