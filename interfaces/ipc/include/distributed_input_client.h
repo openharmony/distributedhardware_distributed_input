f1168767e11ba19e9ce193e52631bbf121624dcd
li-tiangang4 <litiangang4@huawei.com>
2024-04-18 16:29:58 +0800

    清理看板告警
    Signed-off-by: li-tiangang4 <litiangang4@huawei.com>

--- interfaces/ipc/include/distributed_input_client.h
+++ interfaces/ipc/include/distributed_input_client.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -44,6 +44,7 @@ namespace DistributedHardware {
 namespace DistributedInput {
 class DistributedInputClient {
 public:
+    DistributedInputClient();
     ~DistributedInputClient(){};
 
     static DistributedInputClient &GetInstance();
@@ -169,7 +170,6 @@ public:
     };
 
 private:
-    DistributedInputClient();
     bool IsJsonData(std::string strData) const;
     void AddWhiteListInfos(const std::string &deviceId, const std::string &strJson) const;
     void DelWhiteListInfos(const std::string &deviceId) const;
